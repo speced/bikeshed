2509cfac8a8b1b195e9b85755fdea43f20683f4b
Tab Atkins <jackalmage@gmail.com>
2013-07-29 17:31:14 -0700

    Actually add the config manager.

---
+++ lib/config.py
@@ -0,0 +1,22 @@
+debug = False
+quiet = False
+scriptPath = "~"
+doc = None
+textMacros = {}
+dfnTypes = {
+    "propdef":"property",
+    "descdef":"descriptor",
+    "valuedef":"value",
+    "typedef":"type",
+    "at-ruledef":"at-rule",
+    "funcdef":"function",
+    "selectordef":"selector",
+    "interfacedef":"interface",
+    "methoddef":"method",
+    "attrdef":"attribute",
+    "dictdef":"dictionary",
+    "enumdeg":"enum",
+    "constdef":"const",
+    "html-elemdef":"html-element",
+    "html-attrdef":"html-attribute"
+}
