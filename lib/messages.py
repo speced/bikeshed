21f8f6e5d55b8bb8b5ba082e26fd39f2c7801bd2
Tab Atkins <jackalmage@gmail.com>
2013-07-31 15:02:22 -0700

    Automatically dedupe die/warn messages.

--- lib/messages.py
+++ lib/messages.py
@@ -2,15 +2,23 @@ import sys
 from lib.fuckunicode import u
 import lib.config as config
 
+messages = set()
+
 def die(msg, *formatArgs):
     if not config.quiet:
-        print u"\033[1;31mFATAL ERROR:\033[0m "+u(msg).format(*map(u, formatArgs))
+        msg = u"\033[1;31mFATAL ERROR:\033[0m "+u(msg).format(*map(u, formatArgs))
+        if msg not in messages:
+            messages.add(msg)
+            print msg
     if not config.debug:
         sys.exit(1)
 
 def warn(msg, *formatArgs):
     if not config.quiet:
-        print u"\033[1;33mWARNING:\033[0m "+u(msg).format(*map(u, formatArgs))
+        msg = u"\033[1;33mWARNING:\033[0m "+u(msg).format(*map(u, formatArgs))
+        if msg not in messages:
+            messages.add(msg)
+            print msg
 
 def say(msg, *formatArgs):
     if not config.quiet:
