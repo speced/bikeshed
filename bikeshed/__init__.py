5e2c832dfb1ba64e61294398b376b8a82c1429f3
Tab Atkins Jr <jackalmage@gmail.com>
2013-11-13 18:29:00 -0800

    Use the new DSL for railroads rather than direct Python.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -462,16 +462,12 @@ def verifyRequiredMetadata(doc):
 
 def transformRailroad(lines, doc, **kwargs):
     import StringIO
-    from railroaddiagrams import Diagram, Sequence, Choice, Optional, OneOrMore, ZeroOrMore, Terminal, NonTerminal, Comment, Skip
-    from railroaddiagrams import Sequence as And, Choice as Or, Optional as Opt, OneOrMore as Plus, ZeroOrMore as Star, NonTerminal as NT, Comment as C, Sequence as S
+    import railroadparser
     ret = [
         "<div class='railroad'>",
         "<style>svg.railroad-diagram{background-color:hsl(30,20%,95%);}svg.railroad-diagram path{stroke-width:3;stroke:black;fill:rgba(0,0,0,0);}svg.railroad-diagram text{font:bold 14px monospace;text-anchor:middle;}svg.railroad-diagram text.label{text-anchor:start;}svg.railroad-diagram text.comment{font:italic 12px monospace;}svg.railroad-diagram rect{stroke-width:3;stroke:black;fill:hsl(120,100%,90%);}</style>"]
     code = ''.join(lines)
-    try:
-        diagram = eval(code)
-    except Exception, e:
-        die("Error generating a railroad diagram:\n{0}\n{1}", str(e), code.replace('\t', '  '))
+    diagram = railroadparser.parse(code)
     temp = StringIO.StringIO()
     diagram.writeSvg(temp.write)
     ret.append(temp.getvalue())
