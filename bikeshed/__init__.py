ab2bef693f2328d48bd5303da6cec29e96d5f47e
Tab Atkins <jackalmage@gmail.com>
2014-02-13 13:16:45 -0800

    Don't repeat regexes - they'll diverge.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -908,8 +908,8 @@ def classifyLink(el):
     linkType = determineLinkType(el)
     el.set('data-link-type', linkType)
     linkText = determineLinkText(el)
-    if linkType == "maybe" and re.match(r"([\w/-]+)/([\w-]+)", linkText):
-        match = re.match(r"^([\w/-]+)/([\w-]+)$", linkText)
+    match = re.match(r"^([\w/-]+)/([\w-]+)$", linkText)
+    if linkType == "maybe" and match:
         el.set('data-link-for', match.group(1))
         clearContents(el)
         linkText = match.group(2)
