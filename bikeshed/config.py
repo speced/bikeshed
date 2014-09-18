739310fc1d32cd1f423b7e16cefa9ce2d4092c59
Tab Atkins <jackalmage@gmail.com>
2014-09-18 14:14:22 -0700

    Add intersperse() utility function.

--- bikeshed/config.py
+++ bikeshed/config.py
@@ -119,6 +119,13 @@ class HierarchicalNumber(object):
     def __str__(self):
         return self.originalVal
 
+def intersperse(iterable, delimiter):
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimiter
+        yield x
+
 
 
 
