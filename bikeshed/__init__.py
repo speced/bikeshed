c9cdff03530c33ee0c18fad991eed38129bc3586
Tab Atkins <jackalmage@gmail.com>
2015-01-23 13:39:48 -0800

    Dedup IDs for issues that are identical, just don't warn about them.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -613,14 +613,16 @@ def dedupIds(doc, els):
     ids = Counter(el.get('id') for el in findAll("[id]", doc))
     dupes = [id for id,count in ids.items() if count > 1]
     for dupe in dupes:
+        warn = True
         if re.match(r"issue-[0-9a-fA-F]{8}$", dupe):
             # Don't warn about issues, it's okay if they have the same ID because they're identical text.
-            continue
+            warn = False
         els = findAll("#"+dupe, doc)
         ints = iter.imap(str, iter.count(0))
         for el in els[1:]:
             # Try to de-dup the id by appending an integer after it.
-            warn("Multiple elements have the same ID '{0}'.\nDeduping, but this ID may not be stable across revisions.", dupe)
+            if warn:
+                warn("Multiple elements have the same ID '{0}'.\nDeduping, but this ID may not be stable across revisions.", dupe)
             for x in ints:
                 if not findId(dupe+x):
                     el.set("id", dupe+x)
