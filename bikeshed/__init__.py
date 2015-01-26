9fc7296ef564fd02d11b6dc1c3f173a94047f5dc
Tab Atkins <jackalmage@gmail.com>
2015-01-26 11:36:31 -0800

    Dont' overwrite functions with local variables, dummy.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -613,15 +613,15 @@ def dedupIds(doc, els):
     ids = Counter(el.get('id') for el in findAll("[id]", doc))
     dupes = [id for id,count in ids.items() if count > 1]
     for dupe in dupes:
-        warn = True
+        warnAboutDupes = True
         if re.match(r"issue-[0-9a-fA-F]{8}$", dupe):
             # Don't warn about issues, it's okay if they have the same ID because they're identical text.
-            warn = False
+            warnAboutDupes = False
         els = findAll("#"+dupe, doc)
         ints = iter.imap(str, iter.count(0))
         for el in els[1:]:
             # Try to de-dup the id by appending an integer after it.
-            if warn:
+            if warnAboutDupes:
                 warn("Multiple elements have the same ID '{0}'.\nDeduping, but this ID may not be stable across revisions.", dupe)
             for x in ints:
                 if not findId(dupe+x):
