77811facedddda09615866a31cbdeceee4a5020f
Tab Atkins Jr <jackalmage@gmail.com>
2015-05-22 12:07:37 -0700

    Ugh, forgot that self.md.group might be None.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -1650,7 +1650,7 @@ class CSSSpec(object):
         # If that fails, specializes only on the status.
         # If that fails, grabs the most general file.
         # Filenames must be of the format NAME-GROUP-STATUS.include
-        if group is None:
+        if group is None and self.md.group is not None:
             group = self.md.group.lower()
         if status is None:
             status = self.md.status
