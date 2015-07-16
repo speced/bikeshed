0e69c627c740915878b9191fb0b251a70d08f4ca
Tab Atkins <jackalmage@gmail.com>
2015-07-16 13:53:32 -0700

    Last Call doesn't exist any more.

--- bikeshed/issuelist.py
+++ bikeshed/issuelist.py
@@ -148,7 +148,7 @@ def printHeader(outfile, headerInfo):
 
 <h1>{title} Disposition of Comments for {date} {status}</h1>
 
-<p>Last call document: <a href="{url}">{url}</a>
+<p>Review document: <a href="{url}">{url}</a>
 
 <p>Editor's draft: <a href="http://dev.w3.org/csswg/{shortname}/">http://dev.w3.org/csswg/{shortname}/</a>
 
