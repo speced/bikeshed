514576b69c0d077d893bea663d693afa30d0c13f
Tab Atkins <jackalmage@gmail.com>
2015-07-21 11:45:14 -0700

    Use https urls for the w3c-stylesheet-url macro.

--- bikeshed/MetadataManager.py
+++ bikeshed/MetadataManager.py
@@ -318,13 +318,13 @@ class MetadataManager:
             macros["repository"] = self.repository.name
             macros["repositoryurl"] = self.repository.url
         if self.status == "UD":
-            macros["w3c-stylesheet-url"] = "http://www.w3.org/StyleSheets/TR/w3c-unofficial"
+            macros["w3c-stylesheet-url"] = "https://www.w3.org/StyleSheets/TR/w3c-unofficial"
         elif self.status == "FPWD":
-            macros["w3c-stylesheet-url"] = "http://www.w3.org/StyleSheets/TR/W3C-WD"
+            macros["w3c-stylesheet-url"] = "https://www.w3.org/StyleSheets/TR/W3C-WD"
         elif self.status == "FINDING":
-            macros["w3c-stylesheet-url"] = "http://www.w3.org/StyleSheets/TR/W3C-NOTE"
+            macros["w3c-stylesheet-url"] = "https://www.w3.org/StyleSheets/TR/W3C-NOTE"
         else:
-            macros["w3c-stylesheet-url"] = "http://www.w3.org/StyleSheets/TR/W3C-{0}".format(self.status)
+            macros["w3c-stylesheet-url"] = "https://www.w3.org/StyleSheets/TR/W3C-{0}".format(self.status)
         # Custom macros
         for name, text in self.customTextMacros:
             macros[name.lower()] = text
