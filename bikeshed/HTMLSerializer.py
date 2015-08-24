22dbe3b8e635c56042a9209d483e381f377607b7
Tab Atkins Jr <jackalmage@gmail.com>
2015-08-24 15:33:57 +0200

    Changed serialization to be more opinionated and less horribly ugly.

--- bikeshed/HTMLSerializer.py
+++ bikeshed/HTMLSerializer.py
@@ -3,9 +3,10 @@
 from __future__ import division, unicode_literals
 import re
 import StringIO
+from .htmlhelpers import childNodes, isElement, outerHTML
 
 class HTMLSerializer(object):
-	inlineEls = frozenset(["a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i", "b", "u", "mark", "ruby", "bdi", "bdo", "span", "br", "wbr", "img", "meter", "progress"])
+	inlineEls = frozenset(["a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i", "b", "u", "mark", "ruby", "bdi", "bdo", "span", "br", "wbr", "img", "meter", "progress", "[]"])
 	rawEls = frozenset(["xmp", "script", "style"])
 	voidEls = frozenset(["area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"])
 	omitEndTagEls = frozenset(["td", "th", "tr", "thead", "tbody", "tfoot", "li", "dt", "dd"])
@@ -15,6 +16,11 @@ class HTMLSerializer(object):
 	def serialize(self):
 		output = StringIO.StringIO()
 		writer = output.write
+		def write(x):
+			output.write("{")
+			output.write(x)
+			output.write("}")
+			output.write("\n")
 		writer("<!doctype html>")
 		root = self.tree.getroot()
 		self._serializeEl(root, writer)
@@ -22,53 +28,100 @@ class HTMLSerializer(object):
 		output.close()
 		return str
 
-	def _serializeEl(self, el, write, indent=0, pre=False, prevSiblingHadNewline=False):
+	def _serializeEl(self, el, write, indent=0, pre=False, inline=False):
 		def unfuckName(n):
 			# LXML does namespaces stupidly
 			if n.startswith("{"):
 				return n.partition("}")[2]
 			return n
-
-		if el.tag not in self.inlineEls:
-			if not prevSiblingHadNewline:
-				write("\n")
-			write(" "*indent)
-		write("<")
-		write(unfuckName(el.tag))
-		for attrName, attrVal in sorted(el.items()):
-			write(" ")
-			write(unfuckName(attrName))
-			write('="')
-			write(self.escapeAttrVal(attrVal))
-			write('"')
-		write(">")
-		if el.tag == "pre":
-			pre = True
-		if el.text:
-			if el.tag in self.rawEls:
-				write(el.text)
-				# raw elements don't have children, so no need to do this in the next section.
-			else:
-				write(self.escapeText(el.text))
-				prevSiblingHadNewline = el.text.endswith("\n")
-		blockChildren = False
-		for child in el.iterchildren():
-			if not blockChildren and child.tag not in self.inlineEls:
-				blockChildren = True
-			self._serializeEl(child, write, indent+1, prevSiblingHadNewline)
-			if child.tail:
-				if not pre and child.tag not in self.inlineEls:
-					write("\n")
-					write(" "*(indent+1))
-				write(self.escapeText(child.tail))
-				prevSiblingHadNewline = child.tail.endswith("\n")
-		if not (el.tag in self.voidEls or el.tag in self.omitEndTagEls):
-			if not pre and blockChildren:
-				write("\n")
-				write(" "*indent)
+		def groupIntoBlocks(nodes):
+			collect = []
+			for node in nodes:
+				if isElement(node) and node.tag not in self.inlineEls:
+					yield collect
+					collect = []
+					yield node
+					continue
+				else:
+					collect.append(node)
+			yield collect
+		def startTag():
+			if not isElement(el): # Is an array
+				return
+			write("<")
+			write(unfuckName(el.tag))
+			for attrName, attrVal in sorted(el.items()):
+				write(" ")
+				write(unfuckName(attrName))
+				write('="')
+				write(self.escapeAttrVal(attrVal))
+				write('"')
+			write(">")
+		def endTag():
+			if not isElement(el): # Is an array
+				return
 			write("</")
 			write(unfuckName(el.tag))
 			write(">")
+
+		if isElement(el):
+			tag = unfuckName(el.tag)
+		else:
+			# el is an array
+			tag = "[]"
+
+		if tag in self.voidEls:
+			write(" "*indent)
+			startTag()
+			return
+		if pre or tag == "pre":
+			startTag()
+			for node in childNodes(el):
+				if isElement(node):
+					self._serializeEl(node, write, indent=indent, pre=True)
+				else:
+					write(node)
+			endTag()
+			return
+		if inline or tag in self.inlineEls:
+			startTag()
+			for i, node in enumerate(childNodes(el)):
+				if i != 0:
+					write(" ")
+				if isElement(node):
+					self._serializeEl(node, write, inline=inline)
+				else:
+					write(node.strip())
+			endTag()
+			return
+		# Otherwise I'm a block element
+		blocks = list(groupIntoBlocks(childNodes(el, skipWS=True)))
+		if len(blocks) == 1 and not isElement(blocks[0]):
+			# Contains only inlines, print accordingly
+			write(" "*indent)
+			startTag()
+			self._serializeEl(blocks[0], write, inline=True)
+			if el.tag not in self.omitEndTagEls:
+				endTag()
+			return
+		# Otherwise I'm a block that contains at least one block
+		write(" "*indent)
+		startTag()
+		for block in blocks:
+			if isElement(block):
+				write("\n")
+				self._serializeEl(block, write, indent=indent+1)
+			else:
+				# is an array of inlines
+				if len(block) > 0:
+					write("\n")
+					write(" "*(indent+1))
+					self._serializeEl(block, write, inline=True)
+		if el.tag not in self.omitEndTagEls:
+			write("\n")
+			write(" "*indent)
+			endTag()
+		return
 
 	def escapeAttrVal(self, val):
 		return val.replace("&", "&amp;").replace('"', "&quot;")
