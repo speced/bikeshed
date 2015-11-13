a7f3fc9dd740bcda77c70ddfb5cb73b7c7efe11d
Tab Atkins Jr <jackalmage@gmail.com>
2015-11-13 14:50:59 -0600

    Detect and correct when the console doesn't support unicodeo output.

--- bikeshed/messages.py
+++ bikeshed/messages.py
@@ -7,11 +7,22 @@ import config
 
 messages = set()
 
+def p(msg):
+    try:
+        print msg
+    except UnicodeEncodingError:
+        warning = "\033[1;33mWARNING:\033[0m Your console does not understand Unicode.\n  Messages may be slightly corrupted."
+        if warning not in messages:
+            print warning
+            messages.add(warning)
+        print msg.encode("ascii", "xmlcharrefreplace")
+
+
 def die(msg, *formatArgs, **namedArgs):
     msg = "\033[1;31mFATAL ERROR:\033[0m "+msg.format(*formatArgs, **namedArgs)
     if msg not in messages:
         messages.add(msg)
-        print msg
+        p(msg)
     if not config.force:
         sys.exit(1)
 
@@ -20,11 +31,11 @@ def warn(msg, *formatArgs, **namedArgs):
         msg = "\033[1;33mWARNING:\033[0m "+msg.format(*formatArgs, **namedArgs)
         if msg not in messages:
             messages.add(msg)
-            print(msg)
+            p(msg)
 
 def say(msg, *formatArgs, **namedArgs):
     if not config.quiet:
-        print msg.format(*formatArgs, **namedArgs)
+        p(msg.format(*formatArgs, **namedArgs))
 
 def resetSeenMessages():
     global messages
