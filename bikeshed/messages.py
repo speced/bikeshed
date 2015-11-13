aed73ba1f74880a8b2a13567adce87b4e8fc82ce
Tab Atkins Jr <jackalmage@gmail.com>
2015-11-13 14:31:50 -0600

    Remove unused progress() function.

--- bikeshed/messages.py
+++ bikeshed/messages.py
@@ -20,23 +20,11 @@ def warn(msg, *formatArgs, **namedArgs):
         msg = "\033[1;33mWARNING:\033[0m "+msg.format(*formatArgs, **namedArgs)
         if msg not in messages:
             messages.add(msg)
-            print msg
+            print(msg)
 
 def say(msg, *formatArgs, **namedArgs):
     if not config.quiet:
         print msg.format(*formatArgs, **namedArgs)
-
-def progress(msg, val, total):
-    if config.quiet:
-        return
-    barSize = 20
-    fractionDone = val / total
-    hashes = "#" * int(fractionDone*barSize)
-    spaces = " " * (barSize - int(fractionDone*barSize))
-    print "\r{0} [{1}{2}] {3}%".format(msg, hashes, spaces, int(fractionDone*100)),
-    if val == total:
-        print
-    sys.stdout.flush()
 
 def resetSeenMessages():
     global messages
