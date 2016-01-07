d3c3e9402d134d7db5b002a2b8a0885bd7ebcfaa
unknown <arronei@glowsoft1.redmond.corp.microsoft.com>
2016-01-07 11:10:42 -0800

    Switch .strip("|") to [1:]

--- bikeshed/shorthands.py
+++ bikeshed/shorthands.py
@@ -142,7 +142,7 @@ def transformAutolinkShortcuts(doc):
             die("Shorthand {0} gives type as '{1}', but only IDL types are allowed.", match.group(0), match.group(3))
             return E.span(match.group(0))
         if match.group(4) is not None:
-            linkText = match.group(4).strip("|")
+            linkText = match.group(4)[1:]
         else:
             linkText = match.group(2)
         return E.code({"class":"idl"},
@@ -152,7 +152,7 @@ def transformAutolinkShortcuts(doc):
     def elementReplacer(match):
         linkType = "element" if match.group(1) is None else "element-attr"
         if match.group(3) is not None:
-            linkText = match.group(3).strip("|")
+            linkText = match.group(3)[1:]
         else:
             linkText = match.group(2)
         return E.code({},
