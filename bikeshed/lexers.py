ccffd3988f2cae0aef6fddc74658afb1f6ccba56
Tab Atkins Jr <jackalmage@gmail.com>
2016-01-12 16:25:45 -0800

    More tweaks to the CSS lexer.

--- bikeshed/lexers.py
+++ bikeshed/lexers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, unicode_literals
+import re
 from pygments.lexer import *
 from pygments.token import *
 
@@ -7,35 +8,48 @@ class CSSLexer(RegexLexer):
     name = "CSS"
     aliases = ['css']
     filenames = ['*.css']
+    flags=re.DOTALL
 
     tokens = {
         b"root": [
-            (r"\s+", Text),
-            (r"[{}\):;]", Punctuation),
-            (r"@[\w-]+", Name, b"at-prelude"),
-            (r"([^{]+)({)", bygroups(Name.Tag, Punctuation), b"decls"),
-            (r".+", Comment)
+            (r"", Text, b"rules"),
         ],
-        b"at-prelude": [
+        b"comment": [
+            (r"\*/", Comment, b"#pop"),
+            (r".", Comment)
+        ],
+        b"at-rule": [
             include(b"values"),
-            (r";|{", Punctuation, b"#pop")
+            (r";", Punctuation, b"#pop"),
+            (r"{\s*}", Punctuation, b"#pop"),
+            (r"{(?=[^;}]*{)", Punctuation, (b"#pop", b"rules")),
+            (r"{(?=[^{}]*;)", Punctuation, (b"#pop", b"decls")),
+            (r"", Text, b"#pop")
+        ],
+        b"rules": [
+            (r"/\*", Comment, b"comment"),
+            (r"\s+", Text),
+            (r"@[\w-]+", Name, b"at-rule"),
+            (r"}", Punctuation, b"#pop"),
+            (r"([^{]+)({)", bygroups(Name.Tag, Punctuation), b"decls"),
         ],
         b"decls": [
             (r";", Punctuation),
-            (r"@[\w-]+", Name, b"at-prelude"),
-            (r"([\w-]+)\s*(:)", bygroups(Name, Punctuation)),
+            (r"@[\w-]+", Name, b"at-rule"),
+            (r"([\w-]+)\s*(:)", bygroups(Keyword, Punctuation)),
             include(b"values"),
             (r"}", Punctuation, b"#pop"),
             (r".+", Text)
         ],
         b"values": [
-            (r"[),/]", Punctuation),
+            (r"[(),/]", Punctuation),
             (r"(\d+)([\w-]+)", bygroups(Literal.Number, Literal)),
-            (r"\d+", Literal.Number),
+            (r"\d+%?", Literal.Number),
+            (r"(url)(\()([^)]*)(\))", bygroups(Name.Function, Punctuation, Literal.String, Punctuation)),
             (r"([\w-]+)(\()", bygroups(Name.Function, Punctuation)),
             (r"\"[^\"]*\"", Literal.String),
             (r"'[^']*'", Literal.String),
-            (r"#?[\w-]+", Keyword),
-            (r"\s+", Text),
+            (r"#?[\w-]+", Text),
+            (r"\s+", Text)
         ]
     }
