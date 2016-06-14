5ad4667da283881e0ded42077c36ad0df769edd0
Tab Atkins <jackalmage@gmail.com>
2016-06-14 16:03:25 -0700

    Make HierarchicalNumber more robust.

--- bikeshed/config.py
+++ bikeshed/config.py
@@ -150,22 +150,34 @@ testAnnotationURL = "https://test.csswg.org/harness/annotate.js"
 @total_ordering
 class HierarchicalNumber(object):
     def __init__(self, valString):
-        if valString.lower() == "none":
+        if valString.strip().lower() == "none":
             self.nums = None
         else:
-            self.nums = [int(x) for x in re.split(r"\D+", valString)]
+            self.nums = [int(x) for x in re.split(r"\D+", valString) if x != ""]
         self.originalVal = valString
 
     def __nonzero__(self):
         return bool(self.nums)
 
     def __lt__(self, other):
+        # Unlevelled numbers are falsey, and greater than all numbers.
+        if not self and other:
+            return False
+        elif self and not other:
+            return True
+        elif not self and not other:
+            return False
+
         try:
             return self.nums < other.nums
         except AttributeError:
             return self.nums[0] < other
 
     def __eq__(self, other):
+        if (not self and other) or (self and not other):
+            return False
+        if not self and not other:
+            return True
         try:
             return self.nums == other.nums
         except AttributeError:
