071447e50c210fa3f98963dfd98ca210758c395c
Mike West <mike@mikewest.org>
2016-08-01 19:26:42 +0200

    HTTPSify WebAppSec. (#768)
    Note that the link to http://ev.buaa.edu.cn/ in the copyright field
    does not support HTTPS.

--- bikeshed/MetadataManager.py
+++ bikeshed/MetadataManager.py
@@ -218,7 +218,7 @@ class MetadataManager:
             macros["deadline"] = unicode(self.deadline.strftime("{0} %B %Y".format(self.deadline.day)), encoding="utf-8")
             macros["isodeadline"] = unicode(self.deadline.strftime("%Y-%m-%d"), encoding="utf-8")
         if self.status in config.TRStatuses:
-            macros["version"] = "http://www.w3.org/TR/{year}/{status}-{vshortname}-{cdate}/".format(**macros)
+            macros["version"] = "https://www.w3.org/TR/{year}/{status}-{vshortname}-{cdate}/".format(**macros)
         elif self.ED:
             macros["version"] = self.ED
         macros["annotations"] = config.testAnnotationURL
