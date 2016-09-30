9b5c2f9f3397c89e6d5be853589ad04fd594e362
Tab Atkins <jackalmage@gmail.com>
2016-09-30 13:37:07 -0700

    Add nodeIter() for going thru all the nodes of a document one-by-one.

--- bikeshed/htmlhelpers.py
+++ bikeshed/htmlhelpers.py
@@ -346,6 +346,35 @@ def childNodes(parentEl, clear=False, skipOddNodes=True):
     return ret
 
 
+def nodeIter(el, clear=False, skipOddNodes=True):
+    # Iterates thru an element and all its descendants,
+    # yielding up each child node it sees in depth-first order.
+    # (In other words, same as el.iter(),
+    #  but returning nodes+strings rather than the stupid LXML model.)
+    # Takes the same kwargs as childNodes
+    if isinstance(el, basestring):
+        yield el
+        return
+    if isinstance(el, etree._ElementTree):
+        el = el.getroot()
+    text = el.text
+    tail = el.tail
+    if clear:
+        el.text = None
+        el.tail = None
+    yield el
+    if text is not None:
+        yield text
+    for c in childElements(el, tag=None):
+        if skipOddNodes and isOddNode(c):
+            continue
+        # yield from nodeIter(c, clear=clear, skipOddNodes=skipOddNodes)
+        for grandChild in nodeIter(c, clear=clear, skipOddNodes=skipOddNodes):
+            yield grandChild
+    if tail is not None:
+        yield tail
+
+
 def treeAttr(el, attrName):
     # Find the nearest instance of the given attr in the tree
     # Useful for when you can put an attr on an ancestor and apply it to all contents.
