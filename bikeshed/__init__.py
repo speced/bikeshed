4bdd932e006e48268609137d71af422af7eaf33a
Tab Atkins <jackalmage@gmail.com>
2016-10-04 15:48:50 -0700

    Switch the panels to <aside>, put them at the end of <body>, use JS to position them.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -1662,39 +1662,82 @@ def addCanIUsePanels(doc):
 
         addClass(dfn, "caniuse-paneled")
         panel = canIUsePanelFor(codeNameToFullName, featId, feature['stats'], lastUpdated)
-        appendChild(dfn, panel)
+        panel.set("dfn-id", dfn.get("id"))
+        appendChild(doc.body, panel)
         atLeastOnePanel = True
 
     if atLeastOnePanel:
         doc.extraScripts['script-caniuse-panel'] = '''
-        document.body.addEventListener("click", function(e) {
-            if(e.target.classList.contains("caniuse-panel-btn")) {
-                e.target.parentNode.classList.toggle("wrapped");
-            }
-        });
-        '''
-        doc.extraStyles['style-caniuse-panel'] = ''
+            window.addEventListener("load", function(){
+                var panels = [].slice.call(document.querySelectorAll(".caniuse-status"));
+                for(var i = 0; i < panels.length; i++) {
+                    var panel = panels[i];
+                    var dfn = document.querySelector("#" + panel.getAttribute("dfn-id"));
+                    var rect = dfn.getBoundingClientRect();
+                    panel.style.top = (window.scrollY + rect.top) + "px";
+                }
+            });
+            document.body.addEventListener("click", function(e) {
+                if(e.target.classList.contains("caniuse-panel-btn")) {
+                    e.target.parentNode.classList.toggle("wrapped");
+                }
+            });'''
+        doc.extraStyles['style-caniuse-panel'] = '''
+            .caniuse-status { font: 1em sans-serif; width: 9em; padding: 0.3em; position: absolute; z-index: 8; top: auto; right: 0.3em; background: #EEE; color: black; box-shadow: 0 0 3px #999; overflow: hidden; border-collapse: initial; border-spacing: initial; }
+            .caniuse-status.wrapped { width: 1em; height: 1em; }
+            .caniuse-status.wrapped > :not(input) { display: none; }
+            .caniuse-status > input { position: absolute; right: 0; top: 0; width: 1em; height: 1em; border: none; background: transparent; padding: 0; margin: 0; }
+            .caniuse-status > p { font-size: 0.6em; margin: 0; padding: 0; }
+            .caniuse-status > p + p { padding-top: 0.5em; }
+            .caniuse-status > .support { display: block; }
+            .caniuse-status > .support > span { padding: 0.2em 0; display: block; display: table; }
+            .caniuse-status > .support > span.partial { color: #666666; filter: grayscale(50%); }
+            .caniuse-status > .support > span.no { color: #CCCCCC; filter: grayscale(100%); }
+            .caniuse-status > .support > span:first-of-type { padding-top: 0.5em; }
+            .caniuse-status > .support > span > span { padding: 0 0.5em; display: table-cell; vertical-align: top; }
+            .caniuse-status > .support > span > span:first-child { width: 100%; }
+            .caniuse-status > .support > span > span:last-child { width: 100%; white-space: pre; padding: 0; }
+            .caniuse-status > .support > span::before { content: ' '; display: table-cell; min-width: 1.5em; height: 1.5em; background: no-repeat center center; background-size: contain; text-align: right; font-size: 0.75em; font-weight: bold; }
+            .caniuse-status > .support > .and_chr::before { background-image: url(https://resources.whatwg.org/browser-logos/chrome.svg); }
+            .caniuse-status > .support > .and_ff::before { background-image: url(https://resources.whatwg.org/browser-logos/firefox.png); }
+            .caniuse-status > .support > .and_uc::before { background-image: url(https://resources.whatwg.org/browser-logos/uc.png); } /* UC Browser for Android */
+            .caniuse-status > .support > .android::before { background-image: url(https://resources.whatwg.org/browser-logos/android.svg); }
+            .caniuse-status > .support > .bb::before { background-image: url(https://resources.whatwg.org/browser-logos/bb.jpg); } /* Blackberry Browser */
+            .caniuse-status > .support > .chrome::before { background-image: url(https://resources.whatwg.org/browser-logos/chrome.svg); }
+            .caniuse-status > .support > .edge::before { background-image: url(https://resources.whatwg.org/browser-logos/edge.svg); }
+            .caniuse-status > .support > .firefox::before { background-image: url(https://resources.whatwg.org/browser-logos/firefox.png); }
+            .caniuse-status > .support > .ie::before { background-image: url(https://resources.whatwg.org/browser-logos/ie.png); }
+            .caniuse-status > .support > .ie_mob::before { background-image: url(https://resources.whatwg.org/browser-logos/ie-mobile.svg); }
+            .caniuse-status > .support > .ios_saf::before { background-image: url(https://resources.whatwg.org/browser-logos/safari-ios.svg); }
+            .caniuse-status > .support > .op_mini::before { background-image: url(https://resources.whatwg.org/browser-logos/opera-mini.png); }
+            .caniuse-status > .support > .op_mob::before { background-image: url(https://resources.whatwg.org/browser-logos/opera.png); }
+            .caniuse-status > .support > .opera::before { background-image: url(https://resources.whatwg.org/browser-logos/opera.png); }
+            .caniuse-status > .support > .safari::before { background-image: url(https://resources.whatwg.org/browser-logos/safari.png); }
+            .caniuse-status > .caniuse { text-align: right; font-style: italic; }
+            @media (max-width: 767px) {
+                .caniuse-status.wrapped { width: 9em; height: auto; }
+                .caniuse-status:not(.wrapped) { width: 1em; height: 1em; }
+                .caniuse-status.wrapped > :not(input) { display: block; }
+                .caniuse-status:not(.wrapped) > :not(input) { display: none; }
+            }'''
 
 
 def canIUsePanelFor(codeNameToFullName, featId, featStats, lastUpdated):
-    panel = E.div({"class": "status", "data-deco": ""})
-    appendChild(panel, E.input({"value": u"\u22F0", "type": "button", "class":"caniuse-panel-btn"}))
-    mainPara = E.p({"class": "support"})
-    strong = E.strong({}, "Support:")
-    appendChild(mainPara, strong)
+    panel = E.aside({"class": "caniuse-status", "data-deco": ""},
+        E.input({"value": u"\u22F0", "type": "button", "class":"caniuse-panel-btn"}))
+    mainPara = E.p({"class": "support"}, E.b({}, "Support:"))
+    appendChild(panel, mainPara)
     for browserCodeName, browserFullName in codeNameToFullName.iteritems():
         statusCode, minVersion = compatSummaryFor(featStats[browserCodeName])
         if statusCode == "u":
             continue
-        appendChild(
-            mainPara,
+        appendChild(mainPara,
             browserCompatSpan(browserCodeName, browserFullName, statusCode, minVersion))
-    appendChild(panel, mainPara)
-    attribution = E.p({"class": "caniuse"}, "Source: ")
-    anchor = E.a({"href": "http://caniuse.com/#feat=" + featId}, "caniuse.com")
-    anchor.tail = " as of " + lastUpdated
-    appendChild(attribution, anchor)
-    appendChild(panel, attribution)
+    appendChild(panel,
+        E.p({"class": "caniuse"},
+            "Source: ",
+            E.a({"href": "http://caniuse.com/#feat=" + featId}, "caniuse.com"),
+            " as of " + lastUpdated))
     return panel
 
 
@@ -1748,17 +1791,19 @@ def browserCompatSpan(browserCodeName, browserFullName, statusCode, minVersion=N
     # browserCodeName: e.g. and_chr, ios_saf, ie, etc...
     # browserFullName: e.g. "Chrome for Android"
     statusClass = {"y": "yes", "n": "no", "a": "partial"}[statusCode]
-    outer = E.span({"class": "{} {}".format(browserCodeName, statusClass)})
-    nameSpan = E.span({}, browserFullName)
+    outer = E.span({"class": browserCodeName + " " + statusClass})
     if statusCode == "a":
-        nameSpan.tail = " (limited)"
-        nameWrapper = E.span()
-        appendChild(nameWrapper, nameSpan)
-        appendChild(outer, nameWrapper)
+        appendChild(outer,
+            E.span({},
+                E.span({},
+                    browserFullName,
+                    " (limited)")))
     else:
-        appendChild(outer, nameSpan)
-    versionSpan = E.span({}, minVersion)
-    appendChild(outer, versionSpan)
+        appendChild(outer,
+            E.span({}, browserFullName))
+    appendChild(outer,
+        E.span({},
+            minVersion))
     return outer
 
 
