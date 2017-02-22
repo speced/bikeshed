82ede3779de8045d8044ebedaa78fd81cb559a4b
Tab Atkins <jackalmage@gmail.com>
2017-02-22 15:49:08 -0800

    Normalize for values whitespace. Fixes #935.

--- bikeshed/config.py
+++ bikeshed/config.py
@@ -646,6 +646,7 @@ def splitForValues(forValues):
     '''
     if forValues is None:
         return None
+    forValues = re.sub("\s+", " ", forValues)
     return [value.strip() for value in re.split(r',(?![^()]*\))', forValues) if value.strip()]
 
 
