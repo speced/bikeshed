7078a45a9d5371b70f350a0b092b4acc1bc4ba87
Tab Atkins-Bittner <jackalmage@gmail.com>
2018-04-30 11:13:46 -0700

    Listen for non-0 return codes and error out.

--- bikeshed/inlineTags/__init__.py
+++ bikeshed/inlineTags/__init__.py
@@ -15,6 +15,9 @@ def processTags(doc):
 			die("Unknown inline tag '{0}' found:\n  {1}", tag, outerHTML(el), el=el)
 			continue
 		command = doc.md.inlineTagCommands[tag]
-		p = Popen(command, stdin=PIPE, stdout=PIPE, shell=True)
+		p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
 		out,err = p.communicate(innerHTML(el))
+		if p.returncode:
+			die("When trying to process {0}, got return code {1} and the following stderr:\n{2}", outerHTML(el), p.returncode, err)
+			continue
 		replaceContents(el, parseHTML(out))
