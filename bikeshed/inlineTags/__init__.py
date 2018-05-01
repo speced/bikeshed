68301171764f2ba7574cc1c43761b25f60b11a50
Tab Atkins-Bittner <jackalmage@gmail.com>
2018-05-01 15:05:11 -0700

    Encode as utf-8 before shelling out, and decode the return value as utf-8. Closes #1238.

--- bikeshed/inlineTags/__init__.py
+++ bikeshed/inlineTags/__init__.py
@@ -16,8 +16,16 @@ def processTags(doc):
 			continue
 		command = doc.md.inlineTagCommands[tag]
 		p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-		out,err = p.communicate(innerHTML(el))
+		out,err = p.communicate(innerHTML(el).encode('utf-8'))
+		try:
+			out = out.decode("utf-8")
+		except (UnicodeDecodeError,e):
+			die("When trying to process {0}, got invalid unicode in stdout:\n{1}", outerHTML(el), e, el=el)
+		try:
+			err = err.decode("utf-8")
+		except (UnicodeDecodeError,e):
+			die("When trying to process {0}, got invalid unicode in stderr:\n{1}", outerHTML(el), e, el=el)
 		if p.returncode:
-			die("When trying to process {0}, got return code {1} and the following stderr:\n{2}", outerHTML(el), p.returncode, err)
+			die("When trying to process {0}, got return code {1} and the following stderr:\n{2}", outerHTML(el), p.returncode, err, el=el)
 			continue
 		replaceContents(el, parseHTML(out))
