6332d5cd55d47bbd3af2fdaaca0ec31bf83fdc90
Jeremy Roman <jbroman@chromium.org>
2019-02-19 16:58:42 -0500

    Fix pip requirements syntax to include a comma between constraints.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'pygments>=2.3.0<2.4',
-        'lxml>=4.2.5<4.3',
-        'html5lib>=1.0.1<1.1',
-        'cssselect>=1.0.3<1.1',
+        'pygments>=2.3.0,<2.4',
+        'lxml>=4.2.5,<4.3',
+        'html5lib>=1.0.1,<1.1',
+        'cssselect>=1.0.3,<1.1',
     ],
     entry_points={'console_scripts': ['bikeshed = bikeshed:main']},
 )
