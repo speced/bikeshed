614fa8b006bccb8aa9fc7927ebb5ca615dd95946
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-01-14 15:55:30 -0800

    Line.py

--- bikeshed/Line.py
+++ bikeshed/Line.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, unicode_literals
+
 
 from . import attr
 
 @attr.s(slots=True)
 class Line(object):
 	i = attr.ib(validator=[attr.validators.instance_of(int)])
-	text = attr.ib(validator=[attr.validators.instance_of(unicode)])
+	text = attr.ib(validator=[attr.validators.instance_of(str)])
 
 	def __unicode__(self):
 		return self.text
 
 def rectify(lines):
-	if any(isinstance(l, unicode) for l in lines):
+	if any(isinstance(l, str) for l in lines):
 		return [Line(-1, l) for l in lines]
 	return lines
