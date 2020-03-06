70c86369151f7b7b97b78c0f664dba09a4024ca2
Sam Sneddon <me@gsnedders.com>
2020-03-06 17:49:20 +0000

    Allow whitespace before "appendix" in headings (#1607)

--- bikeshed/headings.py
+++ bikeshed/headings.py
@@ -102,7 +102,7 @@ def determineHeadingLevels(doc, headings):
             del header.attrib['data-level']
 
         # If we encounter a no-num or an appendix, don't number it or any in the same section.
-        if hasClass(header, "no-num") or textContent(header)[0:9].lower() == "appendix ":
+        if hasClass(header, "no-num") or textContent(header).lstrip()[0:9].lower() == "appendix ":
             skipLevel = min(level, skipLevel)
             continue
         if skipLevel < level:
