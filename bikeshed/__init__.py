e4ffdf22d9286f77741537adec50ff66905fbf23
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-03-18 16:41:20 -0700

    Add a docs placeholder, and point to it in the message.

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -6,13 +6,13 @@ import platform
 if sys.version_info < (3,):
 	print('''Bikeshed has updated to Python 3, but you are trying to run it with
 Python {}. For instructions on upgrading, please check:
-https://tabatkins.github.io/bikeshed/XXX'''.format(platform.python_version()))
+https://tabatkins.github.io/bikeshed/#installing'''.format(platform.python_version()))
 	sys.exit(1)
 
 if sys.version_info < (3,7):
 	print('''Bikeshed now requires Python 3.7; you are on {}.
 For instructions on how to set up a pyenv with 3.7, see:
-https://tabatkins.github.io/bikeshed/XXX'''.format(platform.python_version()))
+https://tabatkins.github.io/bikeshed/#installing'''.format(platform.python_version()))
 	sys.exit(1)
 
 from .cli import main
