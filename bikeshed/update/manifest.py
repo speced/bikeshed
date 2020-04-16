speced/bikeshed

commit e303d520f54a5fc54b48458a6bb0d8eac3a36ca7

Use requests.get to replace urllib.request.urlopen (#1644)

bikeshed/update/manifest.py
@@ -4,8 +4,7 @@
 import hashlib
 import io
 import os
-import urllib.request, urllib.error, urllib.parse
-from contextlib import closing
+import requests
 from datetime import datetime
 
 from ..messages import *
@@ -95,8 +94,7 @@ def updateByManifest(path, dryRun=False):
         warn("Couldn't find local manifest file.\n{0}", e)
         return False
     try:
-        with closing(urllib.request.urlopen(ghPrefix + "manifest.txt")) as fh:
-            remoteManifest = [str(line, encoding="utf-8") for line in fh]
+        remoteManifest = requests.get(ghPrefix + "manifest.txt").text.splitlines()
     except Exception as e:
         warn("Couldn't download remote manifest file.\n{0}", e)
         return False
@@ -140,8 +138,7 @@ def updateByManifest(path, dryRun=False):
             remotePath = ghPrefix + filePath
             localPath = localizePath(path, filePath)
             try:
-                with closing(urllib.request.urlopen(remotePath)) as fh:
-                    newFile = str(fh.read(), encoding="utf-8")
+                newFile = requests.get(remotePath).text
             except Exception as e:
                 warn("Couldn't download file '{0}'.\n{1}", remotePath, e)
                 return False

bikeshed/unsortedJunk.py
@@ -7,7 +7,7 @@ import json
 import logging
 import os
 import re
-import urllib.request, urllib.parse, urllib.error
+import urllib.parse, urllib.error
 from collections import defaultdict, namedtuple
 from functools import partial as curry
 

bikeshed/update/updateBackRefs.py
@@ -4,7 +4,7 @@ import io
 import json
 import re
 import os
-import urllib.request, urllib.error, urllib.parse
+import urllib.error, urllib.parse
 from collections import defaultdict
 from contextlib import closing
 

bikeshed/update/updateBiblio.py
@@ -4,9 +4,8 @@ import io
 import json
 import re
 import os
-import urllib.request, urllib.error, urllib.parse
+import requests
 from collections import defaultdict
-from contextlib import closing
 
 from .. import biblio
 from ..DefaultOrderedDict import DefaultOrderedDict
@@ -96,19 +95,16 @@ def update(path, dryRun=False):
 
 def getSpecrefData():
     try:
-        with closing(urllib.request.urlopen("https://api.specref.org/bibrefs")) as fh:
-            return str(fh.read(), encoding="utf-8")
-    except urllib.error.URLError:
+        return requests.get("https://api.specref.org/bibrefs").text
+    except requests.exceptions.RequestException:
         # SpecRef uses SNI, which old Pythons (pre-2.7.10) don't understand.
         # First try the older herokuapp.com URL.
         try:
-            with closing(urllib.request.urlopen("https://specref.herokuapp.com/bibrefs")) as fh:
-                return str(fh.read(), encoding="utf-8")
+            return requests.get("https://specref.herokuapp.com/bibrefs").text
         except:
             # Try the CSSWG proxy.
             try:
-                with closing(urllib.request.urlopen("https://api.csswg.org/bibrefs")) as fh:
-                    return str(fh.read(), encoding="utf-8")
+                return requests.get("https://api.csswg.org/bibrefs").text
             except:
                 warn("Your Python is too old (pre-2.7.10) to talk to SpecRef over HTTPS, and something's wrong with the CSSWG proxy as well. Report this to the Bikeshed repo, please?")
                 raise
@@ -120,8 +116,7 @@ def getSpecrefData():
 
 def getWG21Data():
     try:
-        with closing(urllib.request.urlopen("https://wg21.link/specref.json")) as fh:
-            return str(fh.read(), encoding="utf-8")
+        return requests.get("https://wg21.link/specref.json").text
     except Exception as e:
         die("Couldn't download the WG21 biblio data.\n{0}", e)
         return "{}"
@@ -129,8 +124,7 @@ def getWG21Data():
 
 def getCSSWGData():
     try:
-        with closing(urllib.request.urlopen("https://raw.githubusercontent.com/w3c/csswg-drafts/master/biblio.ref")) as fh:
-            return [str(line, encoding="utf-8") for line in fh.readlines()]
+        return requests.get("https://raw.githubusercontent.com/w3c/csswg-drafts/master/biblio.ref").text.splitlines()
     except Exception as e:
         die("Couldn't download the CSSWG biblio data.\n{0}", e)
         return []

bikeshed/update/updateCanIUse.py
@@ -3,9 +3,8 @@
 import io
 import json
 import os
-import urllib.request, urllib.error, urllib.parse
+import requests
 from collections import OrderedDict
-from contextlib import closing
 
 from ..messages import *
 
@@ -13,14 +12,13 @@ from ..messages import *
 def update(path, dryRun=False):
     say("Downloading Can I Use data...")
     try:
-        with closing(urllib.request.urlopen("https://raw.githubusercontent.com/Fyrd/caniuse/master/fulldata-json/data-2.0.json")) as fh:
-            jsonString = fh.read()
+        response = requests.get("https://raw.githubusercontent.com/Fyrd/caniuse/master/fulldata-json/data-2.0.json")
     except Exception as e:
         die("Couldn't download the Can I Use data.\n{0}", e)
         return
 
     try:
-        data = json.loads(jsonString, encoding="utf-8", object_pairs_hook=OrderedDict)
+        data = response.json(encoding="utf-8", object_pairs_hook=OrderedDict)
     except Exception as e:
         die("The Can I Use data wasn't valid JSON for some reason. Try downloading again?\n{0}", e)
         return

bikeshed/update/updateCrossRefs.py
@@ -4,7 +4,6 @@ import io
 import json
 import re
 import os
-import urllib.request, urllib.error, urllib.parse
 from collections import defaultdict
 from contextlib import closing
 from json_home_client import Client as APIClient

bikeshed/update/updateLanguages.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import io
-import json
 import os
-import urllib.request, urllib.error, urllib.parse
-from contextlib import closing
+import requests
 
 from ..messages import *
 
 def update(path, dryRun=False):
     try:
         say("Downloading languages...")
-        with closing(urllib.request.urlopen("https://raw.githubusercontent.com/tabatkins/bikeshed/master/bikeshed/spec-data/readonly/languages.json")) as fh:
-            data = str(fh.read(), encoding="utf-8")
+        data = requests.get("https://raw.githubusercontent.com/tabatkins/bikeshed/master/bikeshed/spec-data/readonly/languages.json").text
     except Exception as e:
         die("Couldn't download languages data.\n{0}", e)
         return

bikeshed/update/updateLinkDefaults.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import io
-import json
 import os
-import urllib.request, urllib.error, urllib.parse
-from contextlib import closing
+import requests
 
 from ..messages import *
 
 def update(path, dryRun=False):
     try:
         say("Downloading link defaults...")
-        with closing(urllib.request.urlopen("https://raw.githubusercontent.com/tabatkins/bikeshed/master/bikeshed/spec-data/readonly/link-defaults.infotree")) as fh:
-            data = str(fh.read(), encoding="utf-8")
+        data = requests.get("https://raw.githubusercontent.com/tabatkins/bikeshed/master/bikeshed/spec-data/readonly/link-defaults.infotree").text
     except Exception as e:
         die("Couldn't download link defaults data.\n{0}", e)
         return

commit 296feef05226be851f3f284ea306ae0b1501427f

Restore newlines when saving the remote manifest.
This was broken in #1644, as the elements of remoteManifest no longer have
traiing newlines.

bikeshed/update/manifest.py
@@ -158,7 +158,7 @@ def updateByManifest(path, dryRun=False):
                 lastMsgTime = currFileTime
         try:
             with io.open(os.path.join(path, "manifest.txt"), 'w', encoding="utf-8") as fh:
-                fh.write("".join(remoteManifest))
+                fh.write("\n".join(remoteManifest))
         except Exception as e:
             warn("Couldn't save new manifest file.\n{0}", e)
             return False