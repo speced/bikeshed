17d5fe84adeb13c5d889536c16cccd3cd861591e
Tab Atkins-Bittner <jackalmage@gmail.com>
2020-08-14 17:34:58 -0700

    Make CSSLexer simpler and more robust.

--- bikeshed/lexers.py
+++ bikeshed/lexers.py
@@ -13,47 +13,24 @@ class CSSLexer(RegexLexer):
 
     tokens = {
         "root": [
-            (r"", Text, "rules"),
+            (r"/\*", Comment, "comment"),
+            (r"@[\w-]+", Name),
+            (r"([+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)[eE][+-]?(?:\d+(?:\.\d+)?|\d*\.\d+))([a-zA-Z-]+|%)", bygroups(Literal.Number, Keyword)),
+            (r"[+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)[eE][+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)", Literal.Number),
+            (r"([+-]?(?:\d+(?:\.\d+)?|\d*\.\d+))([a-zA-Z-]+|%)", bygroups(Literal.Number, Keyword)),
+            (r"[+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)", Literal.Number),
+            (r"(^|{|;)(\s*)([\w-]+)(\s*)(:)", bygroups(Punctuation, Text, Keyword, Text, Punctuation)),
+            (r"(url)(\()([^)]*)(\))", bygroups(Name.Function, Punctuation, Literal.String, Punctuation)),
+            (r"(:?[\w-]+)(\()", bygroups(Name.Function, Punctuation)),
+            (r"\"[^\"]*\"", Literal.String),
+            (r"'[^']*'", Literal.String),
+            (r"#?[\w-]+", Text),
+            (r"[;,(){}\[\]]", Punctuation),
+            (r"\s+", Text),
+            (r".", Text)
         ],
         "comment": [
             (r"\*/", Comment, "#pop"),
             (r".", Comment)
         ],
-        "at-rule": [
-            include("values"),
-            (r";", Punctuation, "#pop"),
-            (r"{\s*}", Punctuation, "#pop"),
-            (r"{(?=[^;}]*{)", Punctuation, ("#pop", "rules")),
-            (r"{(?=[^{}]*;)", Punctuation, ("#pop", "decls")),
-            (r"", Text, "#pop")
-        ],
-        "rules": [
-            (r"/\*", Comment, "comment"),
-            (r"\s+", Text),
-            (r"@[\w-]+", Name, "at-rule"),
-            (r"}", Punctuation, "#pop"),
-            (r"([^{]+)({)", bygroups(Name.Tag, Punctuation), "decls"),
-        ],
-        "decls": [
-            (r";", Punctuation),
-            (r"@[\w-]+", Name, "at-rule"),
-            (r"([\w-]+)\s*(:)", bygroups(Keyword, Punctuation)),
-            include("values"),
-            (r"}", Punctuation, "#pop"),
-            (r".+", Text)
-        ],
-        "values": [
-            (r"/\*", Comment, "comment"),
-            (r"[(),/]", Punctuation),
-            (r"([+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)[eE][+-]?(?:\d+(?:\.\d+)?|\d*\.\d+))([a-zA-Z-]+|%)", bygroups(Literal.Number, Literal)),
-            (r"[+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)[eE][+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)", Literal.Number),
-            (r"([+-]?(?:\d+(?:\.\d+)?|\d*\.\d+))([a-zA-Z-]+|%)", bygroups(Literal.Number, Literal)),
-            (r"[+-]?(?:\d+(?:\.\d+)?|\d*\.\d+)", Literal.Number),
-            (r"(url)(\()([^)]*)(\))", bygroups(Name.Function, Punctuation, Literal.String, Punctuation)),
-            (r"([\w-]+)(\()", bygroups(Name.Function, Punctuation)),
-            (r"\"[^\"]*\"", Literal.String),
-            (r"'[^']*'", Literal.String),
-            (r"#?[\w-]+", Text),
-            (r"\s+", Text)
-        ]
     }
