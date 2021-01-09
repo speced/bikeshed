4359c1d09871f49c05fdddfb93072465d16b270e
Nick Schonning <nschonni@gmail.com>
2021-01-08 20:49:14 -0500

    fix: flake8 E101 for mixed tabs (#1847)

--- bikeshed/__init__.py
+++ bikeshed/__init__.py
@@ -8,8 +8,8 @@ def verify_python_version():
     if sys.version_info < (3,):
         print(
             """Bikeshed has updated to Python 3, but you are trying to run it with
-	Python {}. For instructions on upgrading, please check:
-	https://tabatkins.github.io/bikeshed/#installing""".format(
+    Python {}. For instructions on upgrading, please check:
+    https://tabatkins.github.io/bikeshed/#installing""".format(
                 platform.python_version()
             )
         )
@@ -18,8 +18,8 @@ def verify_python_version():
     if sys.version_info < (3, 7):
         print(
             """Bikeshed now requires Python 3.7; you are on {}.
-	For instructions on how to set up a pyenv with 3.7, see:
-	https://tabatkins.github.io/bikeshed/#installing""".format(
+    For instructions on how to set up a pyenv with 3.7, see:
+    https://tabatkins.github.io/bikeshed/#installing""".format(
                 platform.python_version()
             )
         )
