4717b82c65c79dbd7575d8c16c5233946b401e9d
Tab Atkins-Bittner <jackalmage@gmail.com>
2022-03-10 13:43:21 -0800

    Explicitly type the aliases.

--- bikeshed/t.py
+++ bikeshed/t.py
@@ -23,19 +23,19 @@ from typing import (
 )
 
 from lxml import etree
-from typing_extensions import Literal
+from typing_extensions import Literal, TypeAlias
 
 
-ElementT = etree._Element  # pylint: disable=protected-access
-DocumentT = etree._ElementTree  # pylint: disable=protected-access
-NodeT = Union[str, ElementT]
+ElementT: TypeAlias = etree._Element  # pylint: disable=protected-access
+DocumentT: TypeAlias = etree._ElementTree  # pylint: disable=protected-access
+NodeT: TypeAlias = Union[str, ElementT]
 
 # Can't actually do recursive types yet :(
 # Get as close as possible, but let lists be Any
-NodesT = Union[NodeT, List]
+NodesT: TypeAlias = Union[NodeT, List]
 
 
 if TYPE_CHECKING:
     if "Spec" not in sys.modules:
         from .Spec import Spec  # pylint: disable=cyclic-import
-    SpecType = Union["Spec"]
+    SpecType: TypeAlias = Union["Spec"]
