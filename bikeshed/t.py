44af0bf3ea96a289f31cd60241f2c3a5f016bfa3
Tab Atkins-Bittner <jackalmage@gmail.com>
2022-07-29 17:05:16 -0700

    Looks like TypeAlias is a 3.10 feature

--- bikeshed/t.py
+++ bikeshed/t.py
@@ -34,7 +34,7 @@ if sys.version_info >= (3, 8):
 else:
     from typing_extensions import Literal, Protocol
 
-if sys.version_info >= (3, 9):
+if sys.version_info >= (3, 10):
     from typing import TypeAlias
 else:
     from typing_extensions import TypeAlias
