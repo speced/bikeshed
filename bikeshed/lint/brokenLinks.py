8e8548fa7270305f02e44337add3846c00b9a822
Tab Atkins-Bittner <jackalmage@gmail.com>
2022-09-26 16:09:34 -0700

    Add a timeout to brokenLinks so it won't hang the testsuite.

--- bikeshed/lint/brokenLinks.py
+++ bikeshed/lint/brokenLinks.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import logging
+import time
 
 from .. import h, messages as m, t
 
@@ -15,7 +16,12 @@ def brokenLinks(doc: t.SpecT) -> None:
 
     m.say("Checking links, this may take a while...")
     logging.captureWarnings(True)  # Silence the requests library :/
+    startTime = time.time()
+    globalTimeout = 10 # seconds
     for el in h.findAll("a", doc):
+        if time.time() - startTime > globalTimeout:
+            m.warn(f"Link checking took longer than {globalTimeout} seconds, skipping the rest.")
+            break
         href = el.get("href")
         if not href or href[0] == "#":
             # Local link
@@ -24,10 +30,12 @@ def brokenLinks(doc: t.SpecT) -> None:
             # Can't check mailto links
             continue
         try:
-            res = requests.get(href, verify=False)
+            res = requests.get(href, verify=False, timeout=5)
+        except requests.exceptions.Timeout:
+            m.warn(f"Checking the following link timed out:\n{h.outerHTML(el)}", el=el)
         except Exception as e:
-            m.warn(f"The following link caused an error when I tried to request it:\n{h.outerHTML(el)}\n{e}")
+            m.warn(f"The following link caused an error when I tried to request it:\n{h.outerHTML(el)}\n{e}", el=el)
             continue
         if res.status_code >= 400:
-            m.warn(f"Got a {res.status_code} status when fetching the link for:\n{h.outerHTML(el)}")
+            m.warn(f"Got a {res.status_code} status when fetching the link for:\n{h.outerHTML(el)}", el=el)
     m.say("Done checking links!")
