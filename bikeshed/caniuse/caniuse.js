3f4cf26c579ac9bc87909df2e8df939285eaa254
Daniel LaLiberte <dlaliberte@google.com>
2023-02-14 21:14:33 +0000

    Fixes for caniuse panel positioning

--- bikeshed/caniuse/caniuse.js
+++ bikeshed/caniuse/caniuse.js
@@ -1,13 +1,16 @@
-window.addEventListener("load", function(){
+function repositionCanIUsePanels(){
     var panels = [].slice.call(document.querySelectorAll(".caniuse-status"));
     for(var i = 0; i < panels.length; i++) {
         var panel = panels[i];
         var dfn = document.querySelector("#" + panel.getAttribute("data-dfn-id"));
         var rect = dfn.getBoundingClientRect();
-        console.info('rect', rect);
         panel.style.top = (window.scrollY + rect.top) + "px";
     }
-});
+}
+
+window.addEventListener("load", repositionCanIUsePanels);
+window.addEventListener("resize", repositionCanIUsePanels);
+
 document.body.addEventListener("click", function(e) {
     if(e.target.classList.contains("caniuse-panel-btn")) {
         e.target.parentNode.classList.toggle("wrapped");
