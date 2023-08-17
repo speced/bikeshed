e5ce644d93b7132499c7d822d3a956d9e123bb3b
Tab Atkins-Bittner <jackalmage@gmail.com>
2023-08-17 14:46:31 -0700

    Don't print element directly; the lint() call does that by itself.

--- bikeshed/lint/unusedInternalDfns.py
+++ bikeshed/lint/unusedInternalDfns.py
@@ -26,6 +26,6 @@ def unusedInternalDfns(doc: t.SpecT) -> None:
     for el in noexportDfns:
         if el.get("id") not in localHrefs:
             m.lint(
-                f"Unexported dfn that's not referenced locally - did you mean to export it?\n{h.outerHTML(el)}",
+                f"Unexported dfn that's not referenced locally - did you mean to export it?",
                 el=el,
             )
