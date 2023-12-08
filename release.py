b07715a731699f78d676327cf5a65c79e3d8454e
Tab Atkins-Bittner <jackalmage@gmail.com>
2023-12-08 14:48:13 -0800

    fix release.py

--- release.py
+++ release.py
@@ -69,7 +69,7 @@ def createRelease():
     try:
         # Clear out the build artifacts, build it, upload, and clean up again.
         subprocess.call("rm -r build dist", shell=True)
-        subprocess.check_call("python setup.py sdist bdist_wheel", shell=True)
+        subprocess.check_call("python setup.py sdist", shell=True)
         if options.test:
             subprocess.check_call(
                 " ".join(
