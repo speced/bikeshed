952a784e1146342c38d5210dad6977944a555fc8
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-03-18 17:13:22 -0700

    Remove the now unnecessary functions

--- bikeshed/shorthands/__init__.py
+++ bikeshed/shorthands/__init__.py
@@ -6,7 +6,6 @@ from . import oldShorthands
 
 def run(doc: t.SpecT) -> None:
     oldShorthands.transformShorthandElements(doc)
-    oldShorthands.transformProductionPlaceholders(doc)
     oldShorthands.transformMaybePlaceholders(doc)
     oldShorthands.transformAutolinkShortcuts(doc)
     oldShorthands.transformProductionGrammars(doc)
