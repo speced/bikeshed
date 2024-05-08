01ffc3e755d2c09f753140c3df6d3b46dbd4a58f
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-05-08 10:59:22 -0700

    Observe the implied autolink start/end tags so misnesting error messages will work better.

--- bikeshed/h/parser/parser.py
+++ bikeshed/h/parser/parser.py
@@ -1176,7 +1176,7 @@ def parseCSSPropdesc(s: Stream, start: int) -> Result[SafeText | list[ParserNode
         Result.fail(start)
     innerStart = start + 1
 
-    match, innerEnd = s.matchRe(start+1, AUTOLINK_PROPDESC_RE).vi
+    match, innerEnd = s.matchRe(start + 1, AUTOLINK_PROPDESC_RE).vi
     if match is None:
         return Result.fail(start)
 
@@ -1695,6 +1695,7 @@ def parseLinkText(
     endingLength = len(endingSigil)
     startLine = s.line(start)
     content: list[ParserNode] = []
+    s.observeNode(start, startTag)
     for res in generateResults(s, start):
         value, i = res.vi
         assert value is not None
@@ -1709,7 +1710,9 @@ def parseLinkText(
             )
             return Result.fail(start)
         if s[i : i + endingLength] == endingSigil:
-            content.append(EndTag.fromStream(s, i, i + endingLength, startTag))
+            endTag = EndTag.fromStream(s, i, i + endingLength, startTag)
+            s.observeNode(i, endTag)
+            content.append(endTag)
             return Result(content, i + endingLength)
     m.die(
         f"{startingSigil}...{endingSigil} autolink was opened at {startTag.loc}, and used | to indicate it was providing explicit linktext, but never closed. Either close your autolink, or escape the initial characters that triggered autolink parsing.",
