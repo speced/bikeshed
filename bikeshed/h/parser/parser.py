e03174581d4d1e089bc6030f19167d8471e3ba7e
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-06-07 05:48:50 -0700

    Don't process markdown escapes for italics/bold, since that's still being handled by post-parsing code.

--- bikeshed/h/parser/parser.py
+++ bikeshed/h/parser/parser.py
@@ -2355,7 +2355,8 @@ def parseMarkdownLinkTitle(s: Stream, start: int, startChar: str) -> Result[str]
     title = htmlifyMarkdownEscapes(s[start:i])
     return Result(title, i + 1)
 
-
+# not escaping * or _ currently, so the slash remains until after HTML parsing,
+# because em/strong aren't parsed until then.
 def isMarkdownEscape(s: Stream, start: int) -> bool:
     return s[start] == "\\" and s[start + 1] in (
         "\\",
@@ -2368,7 +2369,7 @@ def isMarkdownEscape(s: Stream, start: int) -> bool:
         "'",
         "(",
         ")",
-        "*",
+        #"*",
         "+",
         ",",
         "-",
@@ -2384,7 +2385,7 @@ def isMarkdownEscape(s: Stream, start: int) -> bool:
         "[",
         "]",
         "^",
-        "_",
+        #"_",
         "`",
         "{",
         "|",
