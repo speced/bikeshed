b1c6c0652a05295839d44ac2aef788ccc67c9b9b
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-08-26 13:58:14 -0700

    Export Doctype in the module

--- bikeshed/doctypes/__init__.py
+++ bikeshed/doctypes/__init__.py
@@ -1,1 +1,1 @@
-from .manager import NIL_GROUP, NIL_ORG, NIL_STATUS, DoctypeManager, Group, Org, Status
+from .manager import NIL_GROUP, NIL_ORG, NIL_STATUS, Doctype, DoctypeManager, Group, Org, Status
