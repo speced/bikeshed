faedbd6e09571438bb935af8807862ce4f0763f2
Tab Atkins-Bittner <jackalmage@gmail.com>
2024-10-31 15:29:00 -0700

    Don't accidentally mutate the key for all following entries using the same term.

--- bikeshed/update/updateCrossRefs.py
+++ bikeshed/update/updateCrossRefs.py
@@ -446,8 +446,8 @@ def writeAnchorsFile(anchors: AnchorsT, path: str) -> set[str]:
         with open(p, "w", encoding="utf-8") as fh:
             for key, entries in sorted(group_anchors.items(), key=lambda x: x[0]):
                 for e in sorted(entries, key=lambda x: x["url"]):
-                    key, displayKey = config.adjustKey(key, e.get("type", ""))
-                    fh.write(key + "\n")
+                    refKey, displayKey = config.adjustKey(key, e.get("type", ""))
+                    fh.write(refKey + "\n")
                     fh.write(displayKey + "\n")
                     for field in [
                         "type",
