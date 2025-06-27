6180073196bc7f2bdf3e54033c5c5ecbe18eb706
Tab Atkins-Bittner <jackalmage@gmail.com>
2025-06-26 17:23:55 -0700

    Better handle the lack of a for

--- bikeshed/cddl.py
+++ bikeshed/cddl.py
@@ -227,7 +227,7 @@ def markupCDDLBlock(pre: t.ElementT, doc: t.SpecT) -> set[t.ElementT]:
         ref = None
         cddlText = None
         for cddlText in (el.get("data-lt") or "").split("|"):
-            linkFors: t.Sequence[str | None] | None = config.splitForValues(el.get("data-cddl-for", ""))
+            linkFors: t.Sequence[str | None] | None = config.splitForValues(el.get("data-cddl-for"))
             if linkFors is None:
                 linkFors = [None]
             for linkFor in linkFors:
@@ -239,11 +239,14 @@ def markupCDDLBlock(pre: t.ElementT, doc: t.SpecT) -> set[t.ElementT]:
                     el=el,
                     error=True,
                 )
-                if ref and not isinstance(ref, str):
+                if isinstance(ref, str):
+                    ref = None
+                if ref:
                     url = ref.url
                     break
             if ref:
                 break
+
         if url is None:
             el.tag = "dfn"
             el.set("data-dfn-type", cddlType)
