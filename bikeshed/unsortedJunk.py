0140d6060f4fd2ac6b9a347d2991d5beaa9a0e76
Tab Atkins-Bittner <jackalmage@gmail.com>
2025-10-23 16:05:41 -0700

    Update ruff to 0.14.2

--- bikeshed/unsortedJunk.py
+++ bikeshed/unsortedJunk.py
@@ -856,7 +856,7 @@ def decorateAutolink(doc: t.SpecT, el: t.ElementT, linkType: str, linkText: str,
 
     # Put an ID on every reference, so I can link to references to a term.
     if el.get("id") is None:
-        unused1, unused2, id = ref.url.partition("#")  # pylint: disable=unused-variable
+        _, _, id = ref.url.partition("#")
         if id:
             el.set("id", f"ref-for-{id}")
         else:
